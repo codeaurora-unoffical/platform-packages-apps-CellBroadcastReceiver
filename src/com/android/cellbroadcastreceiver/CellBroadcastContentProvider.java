cd6b78026e5ceca0376182cf24aa8e726782a4c6
Rika Brooks <rbrooks@codeaurora.org>
2012-09-20 15:52:58 -0700

    CB: Add system property to enable/disable dup detection
    If system property persist.cb.dup_detection is set to false, duplicate
    detection is disabled.  If it is set to true, duplicate detection is
    enabled.  If the system property does not exist, using true/enabled
    as a default value.
    
    (cherry picked from commit 2be7b492e23cff005fdae4860e25d5fb50667834)
    
    Change-Id: I61c1168b3cb913ea13159689b3d0dd3d7c2aeaad
    CRs-Fixed: 401983

--- src/com/android/cellbroadcastreceiver/CellBroadcastContentProvider.java
+++ src/com/android/cellbroadcastreceiver/CellBroadcastContentProvider.java
@@ -27,6 +27,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.SystemProperties;
 import android.provider.Telephony;
 import android.telephony.CellBroadcastMessage;
 import android.text.TextUtils;
@@ -58,6 +59,12 @@ public class CellBroadcastContentProvider extends ContentProvider {
 
     /** MIME type for an individual cell broadcast. */
     private static final String CB_TYPE = "vnd.android.cursor.item/cellbroadcast";
+
+    /** system property to enable/disable broadcast duplicate detecion.  */
+    private static final String CB_DUP_DETECTION = "persist.cb.dup_detection";
+
+    /** Check for system property to enable/disable duplicate detection.  */
+    private boolean mUseDupDetection = SystemProperties.getBoolean(CB_DUP_DETECTION, true);
 
     static {
         sUriMatcher.addURI(CB_AUTHORITY, null, CB_ALL);
@@ -196,6 +203,8 @@ public class CellBroadcastContentProvider extends ContentProvider {
 
     /**
      * Internal method to insert a new Cell Broadcast into the database and notify observers.
+     * If duplicate detection is disabled by the system property {@link #mUseDupDetection}
+     * message will be treated as a new Cell Broadcast.
      * @param message the message to insert
      * @return true if the broadcast is new, false if it's a duplicate broadcast.
      */
@@ -203,38 +212,40 @@ public class CellBroadcastContentProvider extends ContentProvider {
         SQLiteDatabase db = mOpenHelper.getWritableDatabase();
         ContentValues cv = message.getContentValues();
 
-        // Check for existing alert with same serial number and geo scope
-        String serial = cv.getAsString(Telephony.CellBroadcasts.SERIAL_NUMBER);
-        String plmn = cv.getAsString(Telephony.CellBroadcasts.PLMN);
-        String lac = cv.getAsString(Telephony.CellBroadcasts.LAC);
-        String cid = cv.getAsString(Telephony.CellBroadcasts.CID);
-        String selection;
-        String[] selectionArgs;
+        if (mUseDupDetection) {
+            // Check for existing alert with same serial number and geo scope
+            String serial = cv.getAsString(Telephony.CellBroadcasts.SERIAL_NUMBER);
+            String plmn = cv.getAsString(Telephony.CellBroadcasts.PLMN);
+            String lac = cv.getAsString(Telephony.CellBroadcasts.LAC);
+            String cid = cv.getAsString(Telephony.CellBroadcasts.CID);
+            String selection;
+            String[] selectionArgs;
 
-        if (plmn != null) {
-            if (lac != null) {
-                if (cid != null) {
-                    selection = QUERY_BY_SERIAL_PLMN_LAC_CID;
-                    selectionArgs = new String[] {serial, plmn, lac, cid};
+            if (plmn != null) {
+                if (lac != null) {
+                    if (cid != null) {
+                        selection = QUERY_BY_SERIAL_PLMN_LAC_CID;
+                        selectionArgs = new String[] {serial, plmn, lac, cid};
+                    } else {
+                        selection = QUERY_BY_SERIAL_PLMN_LAC;
+                        selectionArgs = new String[] {serial, plmn, lac};
+                    }
                 } else {
-                    selection = QUERY_BY_SERIAL_PLMN_LAC;
-                    selectionArgs = new String[] {serial, plmn, lac};
+                    selection = QUERY_BY_SERIAL_PLMN;
+                    selectionArgs = new String[] {serial, plmn};
                 }
             } else {
-                selection = QUERY_BY_SERIAL_PLMN;
-                selectionArgs = new String[] {serial, plmn};
+                selection = QUERY_BY_SERIAL;
+                selectionArgs = new String[] {serial};
             }
-        } else {
-            selection = QUERY_BY_SERIAL;
-            selectionArgs = new String[] {serial};
-        }
 
-        Cursor c = db.query(CellBroadcastDatabaseHelper.TABLE_NAME, SELECT_ID_COLUMN,
-                selection, selectionArgs, null, null, null);
+            Cursor c = db.query(CellBroadcastDatabaseHelper.TABLE_NAME, SELECT_ID_COLUMN,
+                    selection, selectionArgs, null, null, null);
 
-        if (c.getCount() != 0) {
-            Log.d(TAG, "ignoring dup broadcast serial=" + serial + " found " + c.getCount());
-            return false;
+            if (c.getCount() != 0) {
+                Log.d(TAG, "ignoring dup broadcast serial=" + serial + " found " + c.getCount());
+                return false;
+            }
         }
 
         long rowId = db.insert(CellBroadcastDatabaseHelper.TABLE_NAME, null, cv);
