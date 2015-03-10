CyanogenMod/android_packages_apps_CellBroadcastReceiver

Make Severe alerts dependent on Extreme alerts
As per ATIS-TIA-J-STD-100, Joint CMAS MDB document,
Severe alerts shall be dependent on Extreme.

Unregister Severe alerts along with Extreme alerts,
if Extreme alerts are unchecked.

Bug:18223949
Change-Id: I21e7e062d519ea47be22600481c4d7de2d6185f0

src/com/android/cellbroadcastreceiver/CellBroadcastSettings.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserManager;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -124,6 +125,18 @@ public class CellBroadcastSettings extends PreferenceActivity {
                         @Override
                         public boolean onPreferenceChange(Preference pref, Object newValue) {
                             CellBroadcastReceiver.startConfigService(pref.getContext());
+                            boolean isExtreme =
+                                (pref.getKey()).equals(KEY_ENABLE_CMAS_EXTREME_THREAT_ALERTS);
+                            if (isExtreme) {
+                                boolean isExtremeAlertChecked =
+                                    ((Boolean) newValue).booleanValue();
+                                CheckBoxPreference severeCheckBox = (CheckBoxPreference)
+                                        findPreference(KEY_ENABLE_CMAS_SEVERE_THREAT_ALERTS);
+                                if (severeCheckBox != null) {
+                                    severeCheckBox.setEnabled(isExtremeAlertChecked);
+                                    severeCheckBox.setChecked(false);
+                                }
+                            }
                             return true;
                         }
                     };
@@ -220,6 +233,11 @@ public class CellBroadcastSettings extends PreferenceActivity {
                     findPreference(KEY_ENABLE_CMAS_SEVERE_THREAT_ALERTS);
             if (enableCmasSevereAlerts != null) {
                 enableCmasSevereAlerts.setOnPreferenceChangeListener(startConfigServiceListener);
+                if (enableCmasExtremeAlerts != null) {
+                    boolean isExtremeAlertChecked =
+                            ((CheckBoxPreference)enableCmasExtremeAlerts).isChecked();
+                    enableCmasSevereAlerts.setEnabled(isExtremeAlertChecked);
+                }
             }
             Preference enableCmasAmberAlerts = findPreference(KEY_ENABLE_CMAS_AMBER_ALERTS);
             if (enableCmasAmberAlerts != null) {

codeaurora-unoffical/platform-packages-apps-CellBroadcastReceiver

src/com/android/cellbroadcastreceiver/CellBroadcastSettings.java
@@ -27,6 +27,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.os.UserManager;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -265,6 +266,14 @@ public class CellBroadcastSettings extends PreferenceActivity {
                         editor.putBoolean(KEY_ENABLE_ETWS_TEST_ALERTS
                                 + sPhoneId, Boolean.valueOf((value)));
                     } else if (pref == enableCmasExtremeAlerts) {
+                        boolean isExtremeAlertChecked =
+                                ((Boolean) newValue).booleanValue();
+                        if (enableCmasSevereAlerts != null) {
+                            enableCmasSevereAlerts.setEnabled(isExtremeAlertChecked);
+                            enableCmasSevereAlerts.setChecked(false);
+                            editor.putBoolean(KEY_ENABLE_CMAS_SEVERE_THREAT_ALERTS
+                                    + sPhoneId, Boolean.valueOf((value)));
+                        }
                         editor.putBoolean(KEY_ENABLE_CMAS_EXTREME_THREAT_ALERTS
                                 + sPhoneId, Boolean.valueOf((value)));
                     } else if (pref == enableCmasSevereAlerts) {
@@ -308,7 +317,6 @@ public class CellBroadcastSettings extends PreferenceActivity {
                     editor.commit();
                     return true;
                 }
-            };
 
             // Show extra settings when developer options is enabled in settings.
             boolean enableDevSettings = Settings.Global.getInt(getActivity().getContentResolver(),
@@ -391,6 +399,11 @@ public class CellBroadcastSettings extends PreferenceActivity {
             }
             if (enableCmasSevereAlerts != null) {
                 enableCmasSevereAlerts.setOnPreferenceChangeListener(startConfigServiceListener);
+                if (enableCmasExtremeAlerts != null) {
+                    boolean isExtremeAlertChecked =
+                            ((CheckBoxPreference)enableCmasExtremeAlerts).isChecked();
+                    enableCmasSevereAlerts.setEnabled(isExtremeAlertChecked);
+                }
             }
             if (enableCmasAmberAlerts != null) {
                 enableCmasAmberAlerts.setOnPreferenceChangeListener(startConfigServiceListener);